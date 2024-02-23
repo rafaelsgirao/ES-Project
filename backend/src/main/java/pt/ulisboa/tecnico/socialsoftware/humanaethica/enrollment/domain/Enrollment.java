d60674f0de8049bec6073d0f2867cfcf85896862
Pedro Ribeiro <pedro.melo.ribeiro@tecnico.ulisboa.pt>
2024-02-23 20:26:13 +0000

    feat: Create class Enrollment
    Refs: #31

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/domain/Enrollment.java
@@ -0,0 +1,25 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain;
+
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+
+import java.time.LocalDateTime;
+
+public abstract class Enrollment {
+
+    public abstract int getId();
+    public abstract void setId(Integer id);
+
+    public abstract String getMotivation();
+    public abstract void setMotivation(String motivation);
+
+    public abstract LocalDateTime getEnrollmentDate();
+    public abstract void setEnrollmentDate(LocalDateTime enrollmentDate);
+
+    public abstract Volunteer getVolunteer();
+    public abstract void setVolunteer(Volunteer volunteer);
+
+    public abstract Activity getActivity();
+    public abstract void setActivity(Activity activity);
+
+}
