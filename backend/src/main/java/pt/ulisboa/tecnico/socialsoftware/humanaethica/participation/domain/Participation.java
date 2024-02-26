3272938608fb3a3a2b58a212bbf17443d2134942
João Tiago <joao.leal.tintas@tecnico.ulisboa.pt>
2024-02-26 15:21:00 +0000

    feat: add getters & setters in Participation class

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/domain/Participation.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/domain/Participation.java
@@ -1,12 +1,14 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain;
 
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.repository.ParticipationRepository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -26,13 +28,51 @@ public class Participation  {
     @Column(name = "acceptance_date")
     private LocalDateTime acceptanceDate;
 
-    @ManyToOne
+    @ManyToOne (fetch = FetchType.EAGER)
+    @JoinColumn(name = "activity_id")
     private Activity activity;
 
-    @ManyToOne
+    @ManyToOne (fetch = FetchType.EAGER)
+    @JoinColumn(name = "volunteer_id")
     private Volunteer volunteer;
 
     public Participation(Activity activity, Volunteer volunteer) {
         //TODO
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getAcceptanceDate() {
+        return acceptanceDate;
+    }
+
+    public void setAcceptanceDate(LocalDateTime acceptanceDate) {
+        this.acceptanceDate = acceptanceDate;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public Volunteer getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(Volunteer volunteer) {
+        this.volunteer = volunteer;
     }
 }
