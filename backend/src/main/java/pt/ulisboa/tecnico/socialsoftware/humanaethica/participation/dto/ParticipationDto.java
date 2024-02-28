8571c8d1e4e051a2587a153387fe60f97455bf7d
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2024-02-28 16:22:45 +0000

    feat(class): ParticipationDto: add constructor
    Closes #67

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
@@ -3,4 +3,10 @@ package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto;
 public class ParticipationDto {
 
   public ParticipationDto() {}
+
+  public ParticipationDto(Participation participation) {
+    setId(activity.getId());
+    setActivity(participation.getActivity());
+    setAcceptanceDate(participation.getAcceptanceDate());
+  }
 }
