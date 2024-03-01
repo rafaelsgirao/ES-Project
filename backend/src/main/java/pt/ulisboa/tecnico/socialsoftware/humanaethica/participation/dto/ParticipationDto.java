5b15b4ac41bb2f42c6241228d42b1e82a4e35bb8
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2024-03-01 14:29:56 +0000

    fix(ParticipationDto): add getters & setters
    Closes #68

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
@@ -1,6 +1,16 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto;
 
+import java.time.LocalDateTime;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+
 public class ParticipationDto {
+  private Integer id;
+  private int rating;
+  private LocalDateTime acceptanceDate;
+  private Activity activity;
+  private Volunteer volunteer;
 
   public ParticipationDto() {}
 
@@ -8,5 +18,41 @@ public class ParticipationDto {
     setId(activity.getId());
     setActivity(participation.getActivity());
     setAcceptanceDate(participation.getAcceptanceDate());
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public Integer getRating() {
+    return rating;
+  }
+
+  public void setRating(int rating) {
+    this.rating = rating;
+  }
+
+  public LocalDateTime getAcceptanceDate() {
+    return acceptanceDate;
+  }
+
+  public void setAcceptanceDate(LocalDateTime acceptanceDate) {
+    this.acceptanceDate = acceptanceDate;
+  }
+
+  public void setActivity(Activity activity) {
+    this.activity = activity;
+  }
+
+  public Volunteer getVolunteer() {
+    return volunteer;
+  }
+
+  public void setVolunteer(Volunteer volunteer) {
+    this.volunteer = volunteer;
   }
 }
