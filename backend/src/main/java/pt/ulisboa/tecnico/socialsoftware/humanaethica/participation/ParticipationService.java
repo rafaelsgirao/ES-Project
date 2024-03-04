eea1188674f8211970c7afddd4dbb318cf7e85b0
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2024-03-04 19:22:16 +0000

    fix(ParticipationService): Ensure volunteer with given id exists
    Previously we blindly passed volunteer from ParticipationDto. This changes ensure Volunteer is an existing user
    
    Refs #85

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationService.java
@@ -1,44 +1,55 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation;
 
 import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.ACTIVITY_NOT_FOUND;
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.USER_NOT_FOUND;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.repository.ParticipationRepository;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.repository.ActivityRepository;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.stereotype.Service;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.repository.ActivityRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.repository.ParticipationRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.repository.UserRepository;
 
 @Service
 public class ParticipationService {
-    @Autowired
-    ActivityRepository activityRepository;
-    @Autowired
-    ParticipationRepository participationRepository;
+  @Autowired ActivityRepository activityRepository;
+  @Autowired ParticipationRepository participationRepository;
+  @Autowired UserRepository userRepository;
 
-
-    /*
-     * Nestes testes deve ser verificado que o serviço devolve os valores corretos.
-     *  Também deve ser testado que os valores de entrada são válidos: 
-     *    existe um voluntário associado a volunteerId (que está definido em participationDto), 
-     *    uma actividade associada a activityId e um objeto participationDto.
-     */
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public ParticipationDto createParticipation(Integer activityId, ParticipationDto participationDto) {
-        if (activityId == null) {
-            throw new HEException(ACTIVITY_NOT_FOUND);
-        } 
-        
-        Activity activity = activityRepository.findById(activityId).orElseThrow(() -> new HEException(ACTIVITY_NOT_FOUND, activityId));
-
-        Participation participation = new Participation(participationDto, activity, participationDto.getVolunteer());
-
-        participationRepository.save(participation);
-        return new ParticipationDto(participation);
+  /*
+   * Nestes testes deve ser verificado que o serviço devolve os valores corretos.
+   *  Também deve ser testado que os valores de entrada são válidos:
+   *    uma actividade associada a activityId e um objeto participationDto.
+   */
+  @Transactional(isolation = Isolation.READ_COMMITTED)
+  public ParticipationDto createParticipation(
+      Integer activityId, ParticipationDto participationDto) {
+    if (activityId == null) {
+      throw new HEException(ACTIVITY_NOT_FOUND);
     }
+    Integer volunteerId = participationDto.getVolunteer().getId();
+    if (volunteerId == null) {
+      throw new HEException(USER_NOT_FOUND);
+    }
+    Volunteer volunteer =
+        (Volunteer)
+            userRepository
+                .findById(volunteerId)
+                .orElseThrow(() -> new HEException(USER_NOT_FOUND, volunteerId));
+    Activity activity =
+        activityRepository
+            .findById(activityId)
+            .orElseThrow(() -> new HEException(ACTIVITY_NOT_FOUND, activityId));
+
+    Participation participation = new Participation(participationDto, activity, volunteer);
+
+    participationRepository.save(participation);
+    return new ParticipationDto(participation);
+  }
 }
