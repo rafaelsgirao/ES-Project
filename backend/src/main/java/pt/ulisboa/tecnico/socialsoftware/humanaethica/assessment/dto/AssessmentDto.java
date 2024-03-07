53468cdb524b4aa7e8c893d4b541ce8c081c63eb
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2024-03-07 09:39:00 +0000

    feat: add getters/setters to AssessmentDto class

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/dto/AssessmentDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/dto/AssessmentDto.java
@@ -10,5 +10,44 @@ public class AssessmentDto {
   private UserDto volunteer;
   private InstitutionDto institution;
 
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getReview() {
+    return review;
+  }
+
+  public void setReview(String review) {
+    this.review = review;
+  }
+
+  public String getReviewDate() {
+    return reviewDate;
+  }
+
+  public void setReviewDate(String reviewDate) {
+    this.reviewDate = reviewDate;
+  }
+
+  public UserDto getVolunteer() {
+    return volunteer;
+  }
+
+  public void setVolunteer(UserDto volunteer) {
+    this.volunteer = volunteer;
+  }
+
+  public InstitutionDto getInstitution() {
+    return institution;
+  }
+
+  public void setInstitution(InstitutionDto institution) {
+    this.institution = institution;
+  }
 
 }
