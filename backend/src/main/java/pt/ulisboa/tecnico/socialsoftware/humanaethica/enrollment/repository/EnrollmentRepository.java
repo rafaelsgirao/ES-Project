71935236f7fb08608cf7d5bae43c7ce19569d07a
Pedro Ribeiro <pedro.melo.ribeiro@tecnivo.ulisboa.pt>
2024-03-07 23:19:01 +0000

    feat: List Enrollment: Repository Layer
    Refs: #115

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/repository/EnrollmentRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/repository/EnrollmentRepository.java
@@ -1,13 +1,17 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
 
 @Repository
 @Transactional
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
-    
+    @Query("SELECT e FROM Enrollment e WHERE e.activity.id = :activityId")
+    List<Enrollment> getEnrollmentsByActivityId(Integer activityId);
 }
