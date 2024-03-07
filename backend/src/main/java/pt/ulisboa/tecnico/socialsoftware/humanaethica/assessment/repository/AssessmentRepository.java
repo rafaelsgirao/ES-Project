6865ece58b143333839cf14c77e052c41e208dd3
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2024-03-07 10:07:01 +0000

    feat: add repository to Assessment class - closes (#111)

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/repository/AssessmentRepository.java
@@ -0,0 +1,15 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain.Assessment;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public interface AssessmentRepository extends JpaRepository<Assessment, Integer> {
+    @Query("SELECT a FROM Assessment a WHERE a.institution.id = :institutionId")
+    List<Assessment> findByInstitutionId(Integer institutionId);
+}
