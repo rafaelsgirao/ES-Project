dd36ddbbc88349e8013d13dbc17f99bc6a682df8
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2024-03-07 09:53:36 +0000

    feat: add invariant - review has at least 10 chars - to Assessment class

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/exceptions/ErrorMessage.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/exceptions/ErrorMessage.java
@@ -53,7 +53,12 @@ public enum ErrorMessage {
     // Participation
     DUPLICATE_PARTICIPATION("Participation already exists"),
     ENROLLMENT_PERIOD_NOT_ENDED("Enrollment period is still open"),
-    INVALID_PARTICIPATION("Invalid Participation");
+    INVALID_PARTICIPATION("Invalid Participation"),
+    ACTIVITY_ALREADY_REPORTED("Activity is already reported with name %s"),
+
+    // Assessment
+    ASSESSMENT_REVIEW_TOO_SHORT("Assessment review should have at least ten characters");
+
 
     public final String label;
 
