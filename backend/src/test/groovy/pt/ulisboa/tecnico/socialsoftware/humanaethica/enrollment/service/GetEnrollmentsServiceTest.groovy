be0e1286dcb958bb6b0b071634d9ef6566e4c8ec
Pedro Ribeiro <pedro.melo.ribeiro@tecnivo.ulisboa.pt>
2024-03-08 09:36:16 +0000

    test: Service Tests: List Enrollments - Try to list non-existing activity enrollments
    Refs: #120, #124

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/service/GetEnrollmentsServiceTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/service/GetEnrollmentsServiceTest.groovy
@@ -7,29 +7,52 @@ import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.User
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
 
+import spock.lang.Unroll
 
 @DataJpaTest
 class GetEnrollmentsServiceTest extends SpockTest{
+
+    public static final Integer NO_EXIST = 123
+
+    def activity
+    def activityId
+    def volunteer
+    def volunteer2
+
     def setup(){
         given: "a institution"
         def institution = institutionService.getDemoInstitution()
         and: "a volunteer"
-        def volunteer = createMember(USER_1_NAME, USER_1_USERNAME, USER_1_PASSWORD, USER_1_EMAIL, Member.Type.VOLUNTEER, institution, Member.State.APPROVED)
+        volunteer = createVolunteer(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, AuthUser.Type.NORMAL, User.State.APPROVED, USER_1_PASSWORD)
         and: "another volunteer"
-        def volunteer2 = createMember(USER_2_NAME, USER_2_USERNAME, USER_2_PASSWORD, USER_2_EMAIL, Member.Type.VOLUNTEER, institution, Member.State.APPROVED)
+        volunteer2 = createVolunteer(USER_2_NAME, USER_2_USERNAME, USER_2_EMAIL, AuthUser.Type.NORMAL, User.State.APPROVED, USER_2_PASSWORD)
         and: "an activity"
-        def activity = createActivity(ACTIVITY_NAME_1, IN_TWO_DAYS, IN_THREE_DAYS, IN_ONE_DAY)
+        activity = createActivity(ACTIVITY_NAME_1, IN_TWO_DAYS, IN_THREE_DAYS, IN_ONE_DAY, institution)
         and: "enrollment info"
-        def enrollmentDto = createEnrollmentDto(ENROLLMENT_MOTIVATION,NOW)
+        def enrollmentDto = createEnrollmentDto(ENROLLMENT_MOTIVATION)
         and: 'an enrollment'
-        def enrollment = new Enrollment(enrollmentDto, activity, volunteer)
-        activityRepository.save(enrollment)
+        def enrollment = new Enrollment(enrollmentDto, volunteer, activity)
+        enrollmentRepository.save(enrollment)
         and: 'another enrollment'
         enrollmentDto.motivation = ENROLLMENT_MOTIVATION_2
-        enrollmentDto.enrollmentDate = NOW
-        enrollment = new Enrollment(enrollmentDto, activity, volunteer2)
-        activityRepository.save(enrollment)
+        enrollment = new Enrollment(enrollmentDto, volunteer2, activity)
+        enrollmentRepository.save(enrollment)
+        activityId = activity.getId()
+    }
+
+    @Unroll
+    def "list the enrollments of a non-existing activity"() {
+        when:
+        def result = enrollmentService.getEnrollmentsByActivity(NO_EXIST)
+
+        then:
+        def error = thrown(HEException)
+        error.getErrorMessage() == ErrorMessage.ACTIVITY_NOT_FOUND  
     }
 
     @TestConfiguration
