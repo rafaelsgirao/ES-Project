4192fdb87e5da977c933c49f1e5d9f1b4e0f7e54
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2024-03-08 13:37:49 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/repository/EnrollmentRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/repository/EnrollmentRepository.java
@@ -1,8 +1,6 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
