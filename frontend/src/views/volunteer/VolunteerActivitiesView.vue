6725a0715b9c3fdeef7238dd3ecfbee561f48835
Diogo Rodrigues <diogojarodrigues@tecnico.ulisboa.pt>
2024-03-17 07:16:31 +0000

    feat: Create Button: Apply for an activity in VolunteerActivitiesView Refs: #180, #181

--- frontend/src/views/volunteer/VolunteerActivitiesView.vue
+++ frontend/src/views/volunteer/VolunteerActivitiesView.vue
@@ -40,6 +40,19 @@
             </template>
             <span>Report Activity</span>
           </v-tooltip>
+          <v-tooltip v-if="availableToEnroll()" bottom>
+            <template v-slot:activator="{ on }">
+              <v-icon
+                class="mr-2 action-button"
+                color="blue"
+                v-on="on"
+                data-cy="applyButton"
+                @click="applyForActivity(item)"
+                >fas fa-right-to-bracket</v-icon
+              >
+            </template>
+            <span>Apply for Activity</span>
+          </v-tooltip>
           <v-tooltip v-if="activityHasEnded(item) && volunteerHasParticipated(item) && !volunteerHasAssessedInstitution(item)" bottom>
             <template v-slot:activator="{ on }">
               <v-icon
@@ -178,6 +191,14 @@ export default class VolunteerActivitiesView extends Vue {
         await this.$store.dispatch('error', error);
       }
     }
+  }
+
+  async applyForActivity(activity: Activity) {
+    console.log('Activity: ', activity);
+  }
+
+  availableToEnroll(): boolean {
+    return true;
   }
 
   activityHasEnded(activity: Activity) {
