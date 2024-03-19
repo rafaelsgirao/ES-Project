96018de649fbfac54a6a52d1e88a3e260ecce96d
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2024-03-19 15:49:40 +0000

    feat(frontend/Enrollment): Add Volunteer attribute from constructor json object
    Closes #195

--- frontend/src/models/enrollment/Enrollment.ts
+++ frontend/src/models/enrollment/Enrollment.ts
@@ -1,15 +1,18 @@
 import { ISOtoString } from '@/services/ConvertDateService';
+import User from '../user/User';
 
 export default class Enrollment {
   id: number | null = null;
   motivation!: string;
   enrollmentDateTime!: string;
+  volunteer!: User;
 
   constructor(jsonObj?: Enrollment) {
     if (jsonObj) {
       this.id = jsonObj.id;
       this.motivation = jsonObj.motivation;
       this.enrollmentDateTime = ISOtoString(jsonObj.enrollmentDateTime);
+      this.volunteer = jsonObj.volunteer;
     }
   }
 }
