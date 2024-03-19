ccdfd76874eaee7e5afe8520b32761ea0a13b962
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2024-03-19 15:38:46 +0000

    feat(EnrollmentDto): Add Volunteer's Dto
    Closes #194

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
@@ -1,44 +1,54 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto;
 
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.dto.UserDto;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
 
 public class EnrollmentDto {
-    private Integer id;
-    private String motivation;
+  private Integer id;
+  private String motivation;
+  private UserDto volunteerDto;
 
-    private String enrollmentDateTime;
+  private String enrollmentDateTime;
 
+  public EnrollmentDto() {}
 
-    public EnrollmentDto() {}
+  public EnrollmentDto(Enrollment enrollment) {
+    this.id = enrollment.getId();
+    this.motivation = enrollment.getMotivation();
+    this.enrollmentDateTime = DateHandler.toISOString(enrollment.getEnrollmentDateTime());
+    this.volunteerDto = new UserDto(enrollment.getVolunteer());
+  }
 
-    public EnrollmentDto(Enrollment enrollment) {
-        this.id = enrollment.getId();
-        this.motivation = enrollment.getMotivation();
-        this.enrollmentDateTime = DateHandler.toISOString(enrollment.getEnrollmentDateTime());
-    }
+  public Integer getId() {
+    return id;
+  }
 
-    public Integer getId() {
-        return id;
-    }
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+  public String getMotivation() {
+    return motivation;
+  }
 
-    public String getMotivation() {
-        return motivation;
-    }
+  public UserDto getVolunteer() {
+    return volunteerDto;
+  }
 
-    public void setMotivation(String motivation) {
-        this.motivation = motivation;
-    }
+  public void setVolunteer(UserDto volunteerDto) {
+    this.volunteerDto = volunteerDto;
+  }
 
-    public String getEnrollmentDateTime() {
-        return enrollmentDateTime;
-    }
+  public void setMotivation(String motivation) {
+    this.motivation = motivation;
+  }
 
-    public void setEnrollmentDateTime(String enrollmentDateTime) {
-        this.enrollmentDateTime = enrollmentDateTime;
-    }
+  public String getEnrollmentDateTime() {
+    return enrollmentDateTime;
+  }
+
+  public void setEnrollmentDateTime(String enrollmentDateTime) {
+    this.enrollmentDateTime = enrollmentDateTime;
+  }
 }
