9ee952ef4f84fcd92cbe9223004b342dea96d372
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2024-03-20 12:50:24 +0000

    feat(Enrollment.ts): Add participating attribute
    Closes #206

--- frontend/src/models/enrollment/Enrollment.ts
+++ frontend/src/models/enrollment/Enrollment.ts
@@ -6,6 +6,7 @@ export default class Enrollment {
   motivation!: string;
   enrollmentDateTime!: string;
   volunteer!: User;
+  participating!: boolean;
 
   constructor(jsonObj?: Enrollment) {
     if (jsonObj) {
@@ -13,6 +14,7 @@ export default class Enrollment {
       this.motivation = jsonObj.motivation;
       this.enrollmentDateTime = ISOtoString(jsonObj.enrollmentDateTime);
       this.volunteer = jsonObj.volunteer;
+      this.participating = jsonObj.participating;
     }
   }
 }
