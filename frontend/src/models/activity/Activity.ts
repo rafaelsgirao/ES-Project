80d8f4063683a44e64c1482b14c8d727c1afd650
Joao Tiago <joao.leal.tintas@tecnico.ulisboa.pt>
2024-03-21 10:26:59 +0000

    feat: add column with the number of participants to InstitutionActivitiesView
    Closes #199.

--- frontend/src/models/activity/Activity.ts
+++ frontend/src/models/activity/Activity.ts
@@ -18,6 +18,7 @@ export default class Activity {
   formattedEndingDate!: string;
   applicationDeadline!: string;
   formattedApplicationDeadline!: string;
+  numberOfParticipants!: number;
 
   constructor(jsonObj?: Activity) {
     if (jsonObj) {
@@ -43,6 +44,7 @@ export default class Activity {
         this.formattedApplicationDeadline = ISOtoString(
           jsonObj.applicationDeadline,
         );
+      this.numberOfParticipants = jsonObj.numberOfParticipants;
     }
   }
 }
